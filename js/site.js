6d8d60e7d72b019ca7d6ef25f5241685d1cae8f7
Matías Zozaya <matias.m.zozaya@gmail.com>
2023-10-25 01:47:59 -0300

    New Dolaruco Website

--- js/site.js
+++ js/site.js
@@ -1,92 +1,85 @@
 // Last Prices URL
-var apiURL = 'https://api.bluelytics.com.ar/v2/latest';
-// How many days to fetch
-var days = '10';
-// Past Prices URL
-var pastPricesURL = 'https://api.bluelytics.com.ar/v2/evolution.json?days=' + days;
+var apiURL = 'https://dolarapi.com/v1/dolares';
 
-async function getDolar(todayURL, yesterdayURL) {
-    // Last Prices Fetch
-    const response1 = await fetch(todayURL);
-    var todayData = await response1.json();
+// Global variables
+var progress = 100;
+var counter = 60;
 
-    // Historic Prices Fetch
-    const response2 = await fetch(yesterdayURL);
-    var pastData = await response2.json();
+// Fetch function
+function getDolares() {
+    let dolares = {};
 
-    // Last Price Info Load
-    $("#blueC").html("<span class='text-danger'>" + todayData.blue.value_buy + " (Compra)</span>");
-    $("#blueV").html("<span class='text-success'>" + todayData.blue.value_sell + " (Venta)</span>");
-    $("#blueAvg").html(todayData.blue.value_avg);
+    fetch(apiURL)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(x => {
+            dolares[x.nombre] = {};
+            dolares[x.nombre].compra = x.compra;
+            dolares[x.nombre].venta = x.venta;
+            dolares[x.nombre].fechaActualizacion = new Date(x.fechaActualizacion).toLocaleDateString("es-AR");
+            dolares[x.nombre].horaActualizacion = new Date(x.fechaActualizacion).toLocaleTimeString("es-AR");
+        });
 
-    // Last Update Date
-    var lastDate = new Date(todayData.last_update).toLocaleDateString('es-AR');
-    var lastTime = new Date(todayData.last_update).toLocaleTimeString('es-AR');
-    $("#lastUpdate").html(`<i class="fas fa-calendar mx-2"></i>` + lastDate + `<i class="fas fa-clock mx-2"></i>` + lastTime);
+        //DOM Values Load
+        $("#blueCompra").text("AR$ " + dolares.Blue.compra);
+        $("#blueVenta").text("AR$ " + dolares.Blue.venta);
+        $("#blueUpdate").text(dolares.Blue.fechaActualizacion + " - " + dolares.Blue.horaActualizacion);
+        if(dolares.Blue.compra == null || dolares.Blue.venta == null) $("#blue").addClass("d-none");
 
-    // Price % Variation Span
-    var pVariationAvg = document.getElementById('priceAvgVariation');
-    
-    // Last Prices
-    var priceTodayBuy = todayData.blue.value_buy;
-    var priceTodaySell = todayData.blue.value_sell;
-    var priceTodayAvg = (priceTodayBuy + priceTodaySell) / 2;
+        $("#oficialCompra").text("AR$ " + dolares.Oficial.compra);
+        $("#oficialVenta").text("AR$ " + dolares.Oficial.venta);
+        $("#oficialUpdate").text(dolares.Oficial.fechaActualizacion + " - " + dolares.Oficial.horaActualizacion);
+        if(dolares.Oficial.compra == null || dolares.Oficial.venta == null) $("#oficial").addClass("d-none");
 
-    // Yesterday Prices
-    var priceYesterdayBuy = pastData[3].value_sell;
-    var priceYesterdaySell = pastData[3].value_sell;
-    var priceYesterdayAvg = (priceYesterdayBuy + priceYesterdaySell) / 2;
+        $("#bolsaCompra").text("AR$ " + dolares.Bolsa.compra);
+        $("#bolsaVenta").text("AR$ " + dolares.Bolsa.venta);
+        $("#bolsaUpdate").text(dolares.Bolsa.fechaActualizacion + " - " + dolares.Bolsa.horaActualizacion);
+        if(dolares.Bolsa.compra == null || dolares.Bolsa.venta == null) $("#bolsa").addClass("d-none");
 
-    // Price % Variation Calculation
-    var avgVariation = (priceTodayAvg - priceYesterdayAvg) / priceTodayAvg * 100;
-    avgVariation = avgVariation.toFixed(2);
+        $("#cclCompra").text("AR$ " + dolares["Contado con liquidación"].compra);
+        $("#cclVenta").text("AR$ " + dolares["Contado con liquidación"].venta);
+        $("#cclUpdate").text(dolares["Contado con liquidación"].fechaActualizacion + " - " + dolares["Contado con liquidación"].horaActualizacion);
+        if(dolares["Contado con liquidación"].compra == null || dolares["Contado con liquidación"].venta == null) $("#ccl").addClass("d-none");
 
-    // Price % Variation Print
-    if (priceYesterdayAvg == priceTodayAvg) {
-        pVariationAvg.innerHTML = `<i class="fas fa-minus-circle mr-2"></i> ` + avgVariation + "%";
-        $("#priceAvgVariation").addClass("text-muted");
-        console.log("Yesterday Avg: $" + priceYesterdayAvg);
-        console.log("Today Avg: $" + priceTodayAvg);
-    } else if (priceYesterdayAvg < priceTodayAvg) {
-        pVariationAvg.innerHTML = `<i class="fas fa-chevron-circle-up mr-2"></i> ` + avgVariation + "%";
-        $("#priceAvgVariation").addClass("text-success");
-        console.log("Yesterday Avg: $" + priceYesterdayAvg);
-        console.log("Today Avg: $" + priceTodayAvg);
-    } else if (priceYesterdayAvg > priceTodayAvg) {
-        pVariationAvg.innerHTML = `<i class="fas fa-chevron-circle-down mr-2"></i> ` + avgVariation + "%";
-        $("#priceAvgVariation").addClass("text-danger");
-        console.log("Yesterday Avg: $" + priceYesterdayAvg);
-        console.log("Today Avg: $" + priceTodayAvg);
-    }
+        $("#solidarioCompra").text("AR$ " + dolares["Solidario (Turista)"].compra);
+        $("#solidarioVenta").text("AR$ " + dolares["Solidario (Turista)"].venta);
+        $("#solidarioUpdate").text(dolares["Solidario (Turista)"].fechaActualizacion + " - " + dolares["Solidario (Turista)"].horaActualizacion);
+        if(dolares["Solidario (Turista)"].compra == null || dolares["Solidario (Turista)"].venta == null) $("#solidario").addClass("d-none");
 
-    // Transaction Modal Prices Select
-    $("#tModalBuyValue").attr("value", todayData.blue.value_buy);
-    $("#tModalBuyValue").html("AR$" + todayData.blue.value_buy + " (Compra)");
+        $("#mayoristaCompra").text("AR$ " + dolares.Mayorista.compra);
+        $("#mayoristaVenta").text("AR$ " + dolares.Mayorista.venta);
+        $("#mayoristaUpdate").text(dolares.Mayorista.fechaActualizacion + " - " + dolares.Mayorista.horaActualizacion);
+        if(dolares.Mayorista.compra == null || dolares.Mayorista.venta == null) $("#mayorista").addClass("d-none");
 
-    $("#tModalAverageValue").attr("value", todayData.blue.value_avg);
-    $("#tModalAverageValue").html("AR$" + todayData.blue.value_avg + " (Promedio)");
-
-    $("#tModalSellValue").attr("value", todayData.blue.value_sell);
-    $("#tModalSellValue").html("AR$" + todayData.blue.value_sell + " (Venta)");
-}
-
-getDolar(apiURL, pastPricesURL)
-    .then((data) => {
-        // Spinner hiding
         $(".spinner-border").addClass("d-none");
-        $(".spinner-grow").addClass("d-none");
-        $("#blueAvgContainer").removeClass("d-none");
+        $("#cards-wrapper").removeClass("d-none");
+
+        console.log(dolares);
+        console.log(counter);
     });
-
-// Transaction Calculator
-function tCalc (amount, price) {
-    amount = $('#buyUsdAmount').val();
-    price = $('#buyUsdPrice').val();
-    var result = (amount * price).toLocaleString();
-
-    if (amount != null && amount >= 1) {
-        $("#buyResult").html(`Deberás pagar <span class="text-primary">ARS$ ${result}</span> por los <span class="text-success">USD$ ${amount}</span>`);
-    } else {
-        $("#buyResult").html(`<span class="text-danger">Datos incorrectos.</span> Inténtelo nuevamente.`);
-    }
 };
+getDolares();
+
+// Progress Bar
+setInterval(function() {
+    if(counter == 0) {
+        progress = 100;
+        counter = 60;
+        getDolares()
+    } else {
+        progress = progress -1.66;
+        counter--;
+    };
+
+    $(".progress-bar").css("width", `${progress}` + "%");
+    $(".progress-bar").attr("aria-valuenow", `${progress}`);
+}, 1000);
+
+// MAIN Height calc
+$("main").css("min-height", "calc(100% - " + ($("header").height() + $("footer").height()) + "px)");
+
+$(document).ready(function () {
+    $(window).on("resize", function () {
+        $("main").css("min-height", "calc(100% - " + ($("header").height() + $("footer").height()) + "px)");
+    });
+});
