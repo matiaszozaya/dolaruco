dca2ff4436a9be5fc3e0c4a95cfe55fb48bc1b55
Matías Zozaya <matias.m.zozaya@gmail.com>
2023-10-28 00:42:17 -0300

    Several UI changes & data fix.

--- js/site.js
+++ js/site.js
@@ -24,32 +24,26 @@ function getDolares() {
         $("#blueCompra").text("AR$ " + dolares.Blue.compra);
         $("#blueVenta").text("AR$ " + dolares.Blue.venta);
         $("#blueUpdate").text(dolares.Blue.fechaActualizacion + " - " + dolares.Blue.horaActualizacion);
-        if(dolares.Blue.compra == null || dolares.Blue.venta == null) $("#blue").addClass("d-none");
 
         $("#oficialCompra").text("AR$ " + dolares.Oficial.compra);
         $("#oficialVenta").text("AR$ " + dolares.Oficial.venta);
         $("#oficialUpdate").text(dolares.Oficial.fechaActualizacion + " - " + dolares.Oficial.horaActualizacion);
-        if(dolares.Oficial.compra == null || dolares.Oficial.venta == null) $("#oficial").addClass("d-none");
 
         $("#bolsaCompra").text("AR$ " + dolares.Bolsa.compra);
         $("#bolsaVenta").text("AR$ " + dolares.Bolsa.venta);
         $("#bolsaUpdate").text(dolares.Bolsa.fechaActualizacion + " - " + dolares.Bolsa.horaActualizacion);
-        if(dolares.Bolsa.compra == null || dolares.Bolsa.venta == null) $("#bolsa").addClass("d-none");
 
         $("#cclCompra").text("AR$ " + dolares["Contado con liquidación"].compra);
         $("#cclVenta").text("AR$ " + dolares["Contado con liquidación"].venta);
         $("#cclUpdate").text(dolares["Contado con liquidación"].fechaActualizacion + " - " + dolares["Contado con liquidación"].horaActualizacion);
-        if(dolares["Contado con liquidación"].compra == null || dolares["Contado con liquidación"].venta == null) $("#ccl").addClass("d-none");
 
-        $("#solidarioCompra").text("AR$ " + dolares["Solidario (Turista)"].compra);
+        $("#solidarioCompra").text("AR$ " + dolares.Oficial.compra);
         $("#solidarioVenta").text("AR$ " + dolares["Solidario (Turista)"].venta);
         $("#solidarioUpdate").text(dolares["Solidario (Turista)"].fechaActualizacion + " - " + dolares["Solidario (Turista)"].horaActualizacion);
-        if(dolares["Solidario (Turista)"].compra == null || dolares["Solidario (Turista)"].venta == null) $("#solidario").addClass("d-none");
 
         $("#mayoristaCompra").text("AR$ " + dolares.Mayorista.compra);
         $("#mayoristaVenta").text("AR$ " + dolares.Mayorista.venta);
         $("#mayoristaUpdate").text(dolares.Mayorista.fechaActualizacion + " - " + dolares.Mayorista.horaActualizacion);
-        if(dolares.Mayorista.compra == null || dolares.Mayorista.venta == null) $("#mayorista").addClass("d-none");
 
         $(".spinner-border").addClass("d-none");
         $("#cards-wrapper").removeClass("d-none");
@@ -60,13 +54,14 @@ function getDolares() {
 };
 getDolares();
 
-// Progress Bar
+// Progress Bar & Fetch Reload
 setInterval(function() {
     if(counter == 0) {
         progress = 100;
         counter = 60;
         getDolares()
     } else {
+        $("#counter").text(counter);
         progress = progress -1.66;
         counter--;
     };
@@ -74,6 +69,9 @@ setInterval(function() {
     $(".progress-bar").css("width", `${progress}` + "%");
     $(".progress-bar").attr("aria-valuenow", `${progress}`);
 }, 1000);
+
+// Keep Year Updated on Footer
+$("#year").text(new Date().getFullYear());
 
 // MAIN Height calc
 $("main").css("min-height", "calc(100% - " + ($("header").height() + $("footer").height()) + "px)");
